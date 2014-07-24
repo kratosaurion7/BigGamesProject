e587044b924f197e85e7b217e98a0ea8e9302a52
Tristan Dube <tristan.dube@gmail.com>
2014-07-23 21:19:08 -0400

    Added a requirements.txt file, some support files and cleaned the gane a bit. Switched to the 3.3 environement.
    Might still change back to 2.7 because pika is on 3.3 only.

--- Games/Pong/Python/PongGame/program.py
+++ Games/Pong/Python/PongGame/program.py
@@ -3,41 +3,5 @@ from pygame import *
 
 import Game
 
-#xCoord = 0
-#yCoord = 0
-
-#move = False;
-
-#def MoveRight():
-#    global xCoord
-#    xCoord += 1
-
-#pygame.init()
-
-#xsurface = pygame.display.set_mode((800, 600))
-
-#rec = Surface((50, 50))
-
-#rec.fill((0, 255, 0))
-
-#pygame.display.set_caption("WINDOWZ")
-
-#while True:
-#    for event in pygame.event.get():
-#        if event.type == KEYDOWN:
-#            move = True
-#        if event.type == KEYUP:
-#            move = False
-#        if event.type == QUIT:
-#            pygame.quit()
-#            sys.exit()
-    
-#    if move:
-#        MoveRight()
-
-#    xsurface.fill((0, 0, 0))
-#    xsurface.blit(rec, (xCoord, yCoord))
-#    pygame.display.update()
-
 pongGame = Game.Game()
 pongGame.Start()
